16ca71c55434f3734a3c438e8dd6dfb5bcabe9e8
Alexmacedosantana <alexsantana123455678@gmail.com>
2026-04-13 10:17:47 -0500

    atualização do for com resto

--- src/javascript.js
+++ src/javascript.js
@@ -102,12 +102,20 @@ if (!temCarteira) {
 }
 
 //Etapa 3 for loop
-   // Exercio1
-for (let i = 1; i <= 10; i++){
-   console.log(i);
+// Exercio1
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
 }
-  //exercio2
-  for (let i = 10; i >= 1; i--){
-    console.log(i);
+//exercio2
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
 }
+//exercio3
 
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0){
+    console.log(i);
+  } else {
+    console.log("impar:");
+  }
+}
